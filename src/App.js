9339e15c848a4a6b2ca27b53afc6404d48eb7415
Elahe Hashemi <elahe.hs481@gmail.com>
2023-07-17 19:48:49 +0330

    signup project added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SingUp from './component/SingUp';
+const App = () => {
+    return (
+        <div>
+            <SingUp />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/component/SingUp.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useState } from 'react';
+import styles from './SignUp.module.css';
+
+const SingUp = () => {
+
+const [data, setData] = useState({
+    name:"",
+    email: "",
+    password: "",
+    confirmpassword: "",
+    isAccepted: false
+})
+
+const changeHandler = (event) => {
+    if(event.target.name === "isAccepted") {
+        setData({...data,[event.target.name]: event.target.check})
+    }
+    else {
+        setData({...data, [event.target.name]: event.target.value})
+    }
+}
+
+    return (
+
+        <div>
+
+        <form>
+            <h2>SignUp</h2>
+            <label>Name</label>
+            <input type='text' value={data.name} name='name' onChange={changeHandler} />
+            <label>Email</label>
+            <input type='text' value={data.email} name='email' onChange={changeHandler} />
+
+            <label>Password</label>
+            <input type='password' value={data.password} name='password' onChange={changeHandler} />
+
+            <label>ConfirmPassword</label>
+            <input type='password' value={data.confirmpassword} name='confirmpassword' onChange={changeHandler} />
+
+            <label>I accept a terms of privacy policy</label>
+            <input type='checkbox' value={data.isAccepted} name='isAccepted' onChange={changeHandler} />
+            <div>
+                <a href='#'>Login</a>
+                <button type='submit'>Sign Up</button>
+            </div>
+
+        </form>
+
+
+
+            
+        </div>
+
+
+
+    );
+};
+
+export default SingUp;
