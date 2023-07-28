468bb5b005f33052c6a16c286ffa10b588ecfb17
Elahe Hashemi <elahe.hs481@gmail.com>
2023-07-28 17:41:59 +0330

    shopcart finished

--- src/components/ShopCarts.js
+++ src/components/ShopCarts.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import { CartContext } from '../Context/CardContextProvider';
 import Cart from './shared/Cart';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const ShopCarts = () => {
 
@@ -14,12 +15,30 @@ const ShopCarts = () => {
             {
                 state.itemsCounter > 0 && 
                 <div>
-                   <P><span>Total Items</span></P>     
+                  <p><span>Total Items{state.itemsCounter}</span></p>    
+                  <p><span>Total Payment{state.total}</span></p>
+
+                    <div>
+                        <button onClick={()=> dispatch({type: "CHECKOUT"})}>Check out</button>
+                        <button onClick={()=> dispatch({type: "CLEAR"})}>Clear</button>
+                    </div>
+
 
                 </div>
 
             }
-
+            {
+                state.checkout && <div>
+                    <h3>Checked out successfully</h3>
+                    <Link to="/products">Buy more</Link>
+                </div>
+            }
+              {
+                !state.checkout && state.itemsCounter === 0 && <div>
+                    <h3>Want to Buy?</h3>
+                    <Link to="/products">Go back to shop</Link>
+                </div>
+            }
 
         </div>
     );
